6ba3bbe69ccabe4b734c66d397ad3734e8585413
DJDams1 <dami.scoot3+3@gmail.com>
2025-07-15 21:24:51 +0200

    reel bete0.0.1

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -142,10 +142,10 @@ class Launcher {
                     continue
                 }
                 if (account.meta.type === 'Xbox') {
-                    console.log(`Account Type: ${account.meta.type} | Username: ${account.name}`);
+                    console.log(`Account Type: ${account.meta.type} | Pseudo: ${account.name}`);
                     popupRefresh.openPopup({
                         title: 'Connexion',
-                        content: `Refresh account Type: ${account.meta.type} | Username: ${account.name}`,
+                        content: `Refresh account Type: ${account.meta.type} | Pseudo: ${account.name}`,
                         color: 'var(--color)',
                         background: false
                     });
